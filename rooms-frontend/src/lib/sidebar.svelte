17f515538da9401e42370c0410caf69590ee93f7
Kudyakov Artem <t3m8ch@yandex.ru>
2024-07-23 19:08:56 +0400

    fix: Title link

--- rooms-frontend/src/lib/sidebar.svelte
+++ rooms-frontend/src/lib/sidebar.svelte
@@ -1,6 +1,6 @@
 <aside class="h-screen bg-indigo-600 p-6">
-  <h1 class="font-black text-5xl text-pink-300 hover:text-pink-200 transition duration-100">
-    <a href="/">Rooms</a>
-  </h1>
+  <a href="/" class="block font-black text-5xl text-pink-300 hover:text-pink-200 transition duration-100">
+    Rooms
+  </a>
   <slot></slot>
 </aside>
