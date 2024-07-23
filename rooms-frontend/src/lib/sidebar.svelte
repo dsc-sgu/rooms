b097b9351dd660581f42a5519109308a1cf290a0
Kudyakov Artem <t3m8ch@yandex.ru>
2024-07-23 19:09:25 +0400

    chore: Format

--- rooms-frontend/src/lib/sidebar.svelte
+++ rooms-frontend/src/lib/sidebar.svelte
@@ -1,5 +1,8 @@
 <aside class="h-screen bg-indigo-600 p-6">
-  <a href="/" class="block font-black text-5xl text-pink-300 hover:text-pink-200 transition duration-100">
+  <a
+    href="/"
+    class="block font-black text-5xl text-pink-300 hover:text-pink-200 transition duration-100"
+  >
     Rooms
   </a>
   <slot></slot>
