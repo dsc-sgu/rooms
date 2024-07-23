b6721fc26a3e81d43715cfdabda6a10a2a7a66cc
Kudyakov Artem <t3m8ch@yandex.ru>
2024-07-23 19:22:47 +0400

    feat: Redirect from / to /create-room

--- rooms-frontend/src/routes/+page.svelte
+++ rooms-frontend/src/routes/+page.svelte
@@ -1,20 +1,8 @@
 <script>
-  import UploadRoomImg from '$lib/upload-room-img.svelte';
-  import Wrapper from '$lib/wrapper.svelte';
+  import { goto } from '$app/navigation';
+  import { browser } from '$app/environment';
+
+  if (browser) {
+    goto('/create-room');
+  }
 </script>
-
-<Wrapper>
-  <form class="mt-3 flex flex-col" slot="sidebar">
-    <label for="room-name" class="text-white text-xl mb-2 font-bold">Имя комнаты:</label>
-    <input type="text" id="room-name" class="p-2 font-medium rounded-lg outline-none" />
-    <button
-      type="submit"
-      class="p-2 mt-3 bg-indigo-500 rounded-lg text-xl text-white enabled:hover:bg-indigo-400 disabled:opacity-75 transition duration-100 font-semibold"
-      disabled
-    >
-      Создать комнату
-    </button>
-  </form>
-
-  <UploadRoomImg />
-</Wrapper>
---
+++ rooms-frontend/src/routes/create-room/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+  import UploadRoomImg from '$lib/upload-room-img.svelte';
+  import Wrapper from '$lib/wrapper.svelte';
+</script>
+
+<Wrapper>
+  <form class="mt-3 flex flex-col" slot="sidebar">
+    <label for="room-name" class="text-white text-xl mb-2 font-bold">Имя комнаты:</label>
+    <input type="text" id="room-name" class="p-2 font-medium rounded-lg outline-none" />
+    <button
+      type="submit"
+      class="p-2 mt-3 bg-indigo-500 rounded-lg text-xl text-white enabled:hover:bg-indigo-400 disabled:opacity-75 transition duration-100 font-semibold"
+      disabled
+    >
+      Создать комнату
+    </button>
+  </form>
+
+  <UploadRoomImg />
+</Wrapper>
