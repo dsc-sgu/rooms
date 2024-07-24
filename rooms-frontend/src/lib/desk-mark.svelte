72d36fc88350b9176c1b9250600055a70cff0e38
Kudyakov Artem <t3m8ch@yandex.ru>
2024-07-24 18:59:36 +0400

    feat: Create desk mark component

---
+++ rooms-frontend/src/lib/desk-mark.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  export let num: number;
+  export let isFree: boolean;
+  export let filled: boolean;
+
+  function classes() {
+    if (filled) {
+      return isFree ? 'bg-green-300' : 'bg-amber-300';
+    } else {
+      return (
+        'border-4 ' +
+        (isFree ? 'border-green-400 text-green-400' : 'border-amber-300 text-amber-400')
+      );
+    }
+  }
+</script>
+
+<div
+  class={'w-20 h-20 rounded-full flex justify-center items-center font-bold text-3xl ' + classes()}
+>
+  {num}
+</div>
