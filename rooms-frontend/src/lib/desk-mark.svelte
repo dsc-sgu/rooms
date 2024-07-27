e8b815d647ddeb8358c472914e60633bd1d83531
Kudyakov Artem <t3m8ch@yandex.ru>
2024-07-27 18:07:26 +0400

    style: Add hovers to desk mark component

--- rooms-frontend/src/lib/desk-mark.svelte
+++ rooms-frontend/src/lib/desk-mark.svelte
@@ -5,11 +5,13 @@
 
   function classes() {
     if (filled) {
-      return isFree ? 'bg-green-300 hover:bg-green-200' : 'bg-amber-300';
+      return isFree ? 'bg-green-300 hover:bg-green-200' : 'bg-amber-300 hover:bg-amber-200';
     } else {
       return (
         'border-4 ' +
-        (isFree ? 'border-green-400 text-green-400' : 'border-amber-300 text-amber-400')
+        (isFree
+          ? 'border-green-400 text-green-400 hover:border-green-300 hover:text-green-300'
+          : 'border-amber-400 text-amber-400 hover:border-amber-300 hover:text-amber-300')
       );
     }
   }
